3f959b87f57086b4f4fdde17440df8dc4c0e05cd
AbdullahiKJ <154251356+AbdullahiKJ@users.noreply.github.com>
2026-02-02 18:12:13 +0000

    refactor: update projectile script to accept both collision enter and trigger enter

--- Assets/Scripts/Elements/ElementProjectile.cs
+++ Assets/Scripts/Elements/ElementProjectile.cs
@@ -6,8 +6,31 @@ public class ElementProjectile : MonoBehaviour
 
     void OnCollisionEnter(Collision collision)
     {
-        // Check if the collided object has an IElementReceiver component
-        IElementReceiver receiver = collision.gameObject.GetComponent<IElementReceiver>();
+        HandleCollision(collision, null);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        HandleCollision(null, other);
+    }
+
+    void HandleCollision(Collision collision, Collider other)
+    {
+        // Check if the collided/triggered object has an IElementReceiver component
+        IElementReceiver receiver;
+        if (collision != null)
+        {
+            receiver = collision.gameObject.GetComponent<IElementReceiver>();
+        }
+        else if (other != null)
+        {
+            receiver = other.gameObject.GetComponent<IElementReceiver>();
+        }
+        else
+        {
+            receiver = null;
+        }
+
         if (receiver != null)
         {
             // Receive the incoming element
